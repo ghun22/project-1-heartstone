ea7b3a7d2030d4251fc74b816bc11e5051e6d9be
Gun Lee <ghun22322@gmail.com>
2019-07-08 13:19:16 -0700

    almost done

--- search.js
+++ search.js
@@ -11,38 +11,24 @@ $("#find-Card").on("click", function (event) {
       "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com"
     }
   })
+
+
     .then(function (response) {
-      console.log(response);
-      cardImage = response[0].img;
-      console.log(cardImage);
-      cardName = response[0].name;
-      console.log(cardName);
 
-      $("#movies-view").append(cardName);
-
-      if(cardImage)
+      var findCard = null;
+      for (i=0; i<response.length; i++)
       {
-      $("#title-view").prepend('<img src="https://art.hearthstonejson.com/v1/render/latest/enUS/512x/' + response[0].cardId + '.png">')
+        if(response[i].collectible == true) {
+        findCard =  response[i].cardId;
+        break;
+        }
       }
+      
+    
 
-
-      cardType = response[0].type;
-      console.log(cardType);
-      cardFaction = response[0].faction;
-      console.log(cardFaction);
-      cardRarity = response[0].rarity;
-      console.log(cardRarity);
-      playerClass = response[0].playerClass;
-      console.log(playerClass);
-      artistName = response[0].artist;
-      console.log(artistName);
-
-      //  $("#card-image").attr(src, response.image);
-      $("#card-name").html(response.name);
-      $("#card-type").text(response.type);
-      $("#card-faction").text(response.faction);
-      $("#card-rarity").text(response.rarity);
-      $("#player-class").text(response.playerClass);
-      $("#artist-name").text(response.artist);
+      console.log(response);
+  
+      $("#title-view").prepend('<img src="https://art.hearthstonejson.com/v1/render/latest/enUS/512x/' + findCard   + '.png">')
+      
     })
 })
